6b9ab340f68327a1f6f4386232cdf8573ec81ee3
madhubala140 <madhusekar.s1994@gmail.com>
2023-09-22 12:22:50 +0530

    new updated

--- register-form/script.js
+++ register-form/script.js
@@ -43,10 +43,78 @@
 //     let element=e.target;
 //     console.log(`${element.id} clicked!`);
 // }
+// function setSuccess(element){
+//     const first=element.parentElement;
+//     const errorElement=first.querySelector('.error');
+//     errorElement.innerText='';
+//     first.classList.add('success')
+//     first.classList.remove('error')
+// }
+const form=document.querySelector('#main');
+const username=document.querySelector('#username');
+const email=document.querySelector('#email');
+const password=document.querySelector('#password');
+form.addEventListener('submit',(e)=>{
+    
+    if(!validateInputs()){
+        e.preventDefault();
+    }
+})
+function validateInputs(){
+    const usernameVal=username.value.trim();
+    const emailVal=email.value.trim();
+    const passwordVal=password.value.trim();
+    let success=true;
+
+    if(usernameVal===''){
+        success=false;
+        setError(username,'username is requird')
+    }
+    else{
+        setSuccess(username)
+    }
+    if(emailVal===''){
+        success=false;
+        setError(email,'email is required')
+    }
+    else if(!validateEmail(emailVal)){
+
+        success=false;
+    setError(email,'please enter a valid email')
+}
+else{
+    setSuccess(email)
+}
+if(password===''){
+    setError(password,'password us required')
+}
+else if(passwordVal.length<8){
+    success=false;
+    setError(password,'password atleast 8 charter long')
+}
+else{
+    setSuccess(password)
+}
+return success;
+}
+function setError(element,message){
+    const first=element.parentElement;
+    const errorElement=first.querySelector('.error');
+    errorElement.innerText=message;
+    first.classList.add('error');
+    first.classList.remove('success');
+}
 function setSuccess(element){
     const first=element.parentElement;
     const errorElement=first.querySelector('.error');
     errorElement.innerText='';
-    first.classList.add('success')
-    first.classList.remove('error')
+    first.classList.add('success');
+    first.classList.remove('error');
 }
+const validateEmail = (email) => {
+    return String(email)
+      .toLowerCase()
+      .match(
+        /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+      );
+  };
